ef3c2c8f0bf4f7b00bc0f7eac3953c1462a7ad07
SFPMEDIA <s-pauck@hotmail.com>
2021-02-12 15:13:49 +0100

    Added some functionality

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,19 @@
+import { useState } from 'react';
+
 const Home = () => {
+    //let name = "mario";
+    useState("mario");
+    const [name, setName] = useState("mario");
+    const handleClick = () => {
+        setName("luigi");
+    }
+
+    
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <p>{ name }</p>
+            <button onClick={handleClick}>Click me</button>
         </div>
      );
 }
