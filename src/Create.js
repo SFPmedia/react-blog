b60e184bb78ecea6fc29acd8866f77567e185072
SFPMEDIA <s-pauck@hotmail.com>
2021-02-16 18:23:36 +0100

    Added redirection once a blog has been added

--- src/Create.js
+++ src/Create.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useHistory } from 'react-router-dom';
 
 
 const Create = () => {
@@ -7,6 +8,7 @@ const Create = () => {
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('mario');
     const [isLoading, setIsLoading] = useState(false);
+    const history = useHistory();
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -21,6 +23,7 @@ const Create = () => {
         }).then(() => {
             console.log("new blog added");
             setIsLoading(false);
+            history.push('/');
         })
     }
 
